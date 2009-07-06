0f883c665e76a9f75f00e5a03e9ae4c3fb495e9f
Luis Lavena <luislavena@gmail.com>
2009-07-06 00:48:57 -0300

    Bumped gem version to 2.8.1.1

--- lib/mysql.rb
+++ lib/mysql.rb
@@ -10,7 +10,7 @@ end
 
 # define version string to be used internally for the Gem by Hoe.
 class Mysql
-  module HoeVersion
-    VERSION = '2.8.1'
+  module GemVersion
+    VERSION = '2.8.1.1'
   end
 end
